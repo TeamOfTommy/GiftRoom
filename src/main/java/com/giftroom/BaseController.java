6adccfef5cff608397464c0813a572b80e2dbb09
joyang1 <tingzai.yang@gmail.com>
2017-10-21 18:45:13 +0800

    添加giftroom server后端api架构

---
+++ src/main/java/com/giftroom/BaseController.java
@@ -0,0 +1,4 @@
+package com.giftroom;
+
+public class BaseController {
+}
---
+++ src/main/java/com/giftroom/UserApi/UserApiController.java
@@ -0,0 +1,9 @@
+package com.giftroom.UserApi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("UserApi")
+public class UserApiController {
+}
